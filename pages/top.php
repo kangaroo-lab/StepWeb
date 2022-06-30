4c144ff0867b604f5afb8d0ebec6e46d9476ca37
kangaroo-lab <kobano@icloud.com>
2022-06-30 12:39:35 +0900

    add

--- pages/top.php
+++ pages/top.php
@@ -17,7 +17,7 @@
         $db = "heroku_410d64a133afea6";
 
         $pdo = new PDO(
-          'mysql:host=' . $server . ';dbname=' . $db . ';charset=utf8mb4',
+          'mysql:host='.$server.';dbname='.$db.';charset=utf8mb4',
           $username,
           $password,
           [
